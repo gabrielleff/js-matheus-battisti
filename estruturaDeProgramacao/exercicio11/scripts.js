ad8011d658aa1026f972278fd165571825b37606
Gabrielle <gabriellefelixferr@gmail.com>
2024-05-25 19:10:27 -0300

    adicionando mais dois exercicios de estrutura

---
+++ estruturaDeProgramacao/exercicio11/scripts.js
@@ -0,0 +1,10 @@
+let idadeUsuario = 17;
+let cnh = false;
+
+if(idadeUsuario >= 18 && cnh == true) {
+    console.log("Sim, pode dirigir");
+} else if(idadeUsuario >= 18 && cnh == false) {
+    console.log("Não pode dirigir");
+} else {
+    console.log("Não pode dirigir");
+}
